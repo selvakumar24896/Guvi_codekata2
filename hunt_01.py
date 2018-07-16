b35d1a8a0e5dd9eacf2bce33a1f5c5b43bc9e8ef
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-16 16:55:34 +0530

    Update hunt_01.py

--- hunt_01.py
+++ hunt_01.py
@@ -3,10 +3,16 @@ def check_repeated(inp):
 	for i in range(0,len(inp)):
 		for j in range(i+1,len(inp)):
 			if inp[i]==inp[j]:
-				out.append(inp[i])
+				if inp[i] not in out:
+					out.append(inp[i])
 	print(out)
-n=int(raw_input())
-inp=[]
-for x in range(n):
-	inp.append(int(raw_input()))
-check_repeated(inp)
+	
+def main():
+	n=int(raw_input())
+	inp=[]
+	for x in range(n):
+		inp.append(int(raw_input()))
+	check_repeated(inp)
+	
+if __name__=="__main__":
+	main()
