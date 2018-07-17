afd900121cdc184725e82dc703f900e5f0994876
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-17 15:55:15 +0530

    Add files via upload

---
+++ pro_01.java
@@ -0,0 +1,37 @@
+import java.util.;
+public class Longest_prefix {
+
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int n=inp.nextInt();
+        String[] names=new String[n];
+        for(int i=0;in;i++){
+            names[i]=inp.next();
+            
+        }
+     
+        String result=;
+        for(int i=0;inames.length-1;i++){
+            result=check(names[i],names[i+1]);
+        }
+        System.out.println(result);
+    }
+    public static String check(String str1,String str2){
+        String out=,temp;
+        if(str1.length()str2.length()){
+            temp=str1;
+            str1=str2;
+            str2=temp;
+        }
+        for(int i=0;istr1.length();i++){
+            
+                if(str1.charAt(i) == str2.charAt(i)){
+                    out+=str1.charAt(i);
+                }
+                else
+                    break;
+            }
+        return out;
+        }
+    }
+    
