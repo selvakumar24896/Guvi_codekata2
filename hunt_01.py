8845bae0537392992223ba96de51f4aff3b83143
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-17 12:32:58 +0530

    Update hunt_01.py

--- hunt_01.py
+++ hunt_01.py
@@ -7,12 +7,9 @@ def check_repeated(inp):
 					out.append(inp[i])
 	print(out)
 	
-def main():
-	n=int(raw_input())
-	inp=[]
-	for x in range(n):
-		inp.append(int(raw_input()))
-	check_repeated(inp)
-	
-if __name__=="__main__":
-	main()
+n=int(raw_input())
+inp=[]
+for x in range(n):
+	inp.append(int(raw_input()))
+check_repeated(inp)
+
