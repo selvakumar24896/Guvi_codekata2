1309c17d98b8c8edd8f4b471a18189d421dc4ee1
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-17 12:30:31 +0530

    Update hunt_02.py

--- hunt_02.py
+++ hunt_02.py
@@ -3,12 +3,10 @@ def largenum(inp):
 	inp=sorted(inp)
 	inp.reverse()
 	print(inp)
-def main():
-	n=int(raw_input())
-	inp=[]
-	for x in range(n):
-		inp.append(int(raw_input()))
-	largenum(inp)
+n=int(raw_input())
+inp=[]
+for x in range(n):
+	inp.append(int(raw_input()))
+largenum(inp)
 	
-if __name__=="__main__":
-	main()
+
