de622d3a3b2bebea0b208e358df62ce57129e7e3
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-19 06:32:38 +0530

    Update hunt_06.py

--- hunt_06.py
+++ hunt_06.py
@@ -1,19 +1,25 @@
 def check (li):
 	rp=None
-	p1=None
-	p2=None
-	for x in range(0,len(li)):
+	p1=0
+	p2=0
+	p3=None
+	count=0
+	for x in range(0,len(li)-1):
 		for y in range(x+1,len(li)):
 			if li[x]==li[y]:
 				rp=li[x]
+				
 	if rp is None:
 		return "unique"
 	else:
 		return rp
 
+
+
 n = int(raw_input())
 li = []
 if n>0:
+
 	for x in range(0,n):
 		li.append(int(raw_input()))
 
