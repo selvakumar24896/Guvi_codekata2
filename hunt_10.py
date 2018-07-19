b95ef3ed702525e7e7fc534ec5ef04fd580d652c
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-19 16:05:11 +0530

    Add files via upload

---
+++ hunt_10.py
@@ -0,0 +1,26 @@
+
+n=input()
+m=input()
+if m>0 and n>0:
+    a=[]
+    b=[]
+    res=[]
+    for x in range(0,n):
+        a.append(input())
+    for y in range(0,m):
+        b.append(input())
+
+
+    for y in range(0,m):
+        if b[y] in a:
+            res.append(b[y])
+            a.remove(b[y])
+    if b==res:
+        print "YES"
+    else:
+        print "NO"
+else:
+    print "invalid input"
+
+        
+    
