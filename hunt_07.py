9d1925299caee32409469628d322d3cc1dc10e6c
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-19 11:10:18 +0530

    Add files via upload

---
+++ hunt_07.py
@@ -0,0 +1,17 @@
+n=input()
+if n>0:
+    inp=[]
+    out=[]
+    for i in range(0,n):
+        inp.append(input())
+        print(' inp[i]=',inp[i],' i=',i)
+        if inp[i]%2!=0 and i%2==0:
+            out.append(inp[i])
+        if inp[i]%2==0 and i%2!=0:
+            out.append(inp[i])
+
+
+    print(out)
+else:
+    print("invalid input")
+        
