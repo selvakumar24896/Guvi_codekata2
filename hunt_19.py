0f8ef052cb590021ab2b5fe542bdcdd6ee1e03fd
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-23 15:18:11 +0530

    Add files via upload

---
+++ hunt_19.py
@@ -0,0 +1,29 @@
+import copy
+
+def get(n):
+    vl=[]
+    for i in range(0,n):
+        vl.append(input())
+    return vl
+
+
+def check(l1,l2):
+	res=[]
+	for i in range(0,len(l1)):
+		if l1[i] in l2:
+			res.append((l1[i]))
+	return res
+            
+        
+a=input()
+n=input()
+li=[]
+out=''
+for x in range(0,a):
+    li.append(get(n))
+
+out=list(li[0])
+for x in range(0,(a-1)):
+	out=check(out,li[x+1])
+print out
+    
