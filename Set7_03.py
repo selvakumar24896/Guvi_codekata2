6f88995c9b07117a1f678e13a66b0e74843ff26d
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-24 14:46:34 +0530

    Update Set7_03.py

--- Set7_03.py
+++ Set7_03.py
@@ -1,11 +1,14 @@
 inp=raw_input()
 out=''
-for x in range(0,len(inp)-1):
-	if inp[x]!=inp[x+1] and inp[x] not in out:
-		out+=inp[x]
-	else:
-		continue
-	if inp[x+1] not in out:
-		out+=inp[x+1]
+if len(inp)==1:
+		out=inp;
+else:
+	for x in range(0,len(inp)-1):
+		if inp[x]!=inp[x+1] and inp[x] not in out:
+			out+=inp[x]
+		else:
+			continue
+		if inp[x+1] not in out:
+			out+=inp[x+1]
 
 print len(out)
