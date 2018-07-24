0a5aca5abf365c0ca75b2dda219a47fa4f65797d
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-24 10:51:51 +0530

    Update Set7_01.java

--- Set7_01.java
+++ Set7_01.java
@@ -13,6 +13,7 @@ public class Set7_01 {
         String inp1 = val.next();
         String inp2 = val.next();
         int[] ar=new int[inp1.length()];
+        if(inp1.length()==inp2.length()){
         for(int i=0; i<inp1.length(); i++){
          ar[i] = ((int)inp1.charAt(i)-96)+((int)inp2.charAt(i)-96);
          if( ar[i]>26 ){
@@ -20,6 +21,9 @@ public class Set7_01 {
              if( ar[i]==0 )
                  ar[i] = 26;
          }
+        }
+        }else{
+            System.out.print("GIVE INPUTS WITH SAME LENGTH");
         }
         for(int i=0;i<ar.length;i++){
             System.out.print((char)(ar[i]+96));
