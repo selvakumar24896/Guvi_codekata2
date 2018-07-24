b3a2fb398fa67cb4651c47ae9c9a1c2a94b5eb4d
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-24 14:38:17 +0530

    Add files via upload

---
+++ Set7_03.py
@@ -0,0 +1,11 @@
+inp=raw_input()
+out=''
+for x in range(0,len(inp)-1):
+	if inp[x]!=inp[x+1] and inp[x] not in out:
+		out+=inp[x]
+	else:
+		continue
+	if inp[x+1] not in out:
+		out+=inp[x+1]
+
+print len(out)
