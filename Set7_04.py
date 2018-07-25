303fba057dbcd220ce4ae937b75507737d1d62fc
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-25 14:06:22 +0530

    Update Set7_04.py

--- Set7_04.py
+++ Set7_04.py
@@ -6,8 +6,10 @@ def possibility(n,k):
 n=input("Total no appartments ")
 k=input("inhabited appartments ")
 if n>0:
-	if k<=n:
-		print '1 ',possibility(n,k)
+	if k<n:
+		print '1',possibility(n,k)
+	elif k==n:
+		print '0 '*2
 	else:
 		print "inhabited appartments are greater than Total"
 else:
