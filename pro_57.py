ddebe1f44a342bfb53424552be1ada8ea71679a3
selvakumar24896 <selvakumar24896@gmail.com>
2018-07-30 10:55:28 +0530

    Update pro_57.py

--- pro_57.py
+++ pro_57.py
@@ -5,7 +5,6 @@ temp2=0
 for x in range(0,len(str1)-1):
 	temp=x
 	for y in range(0,len(str2)-1):
-		print "str1=",str1[x]," str2",str2[y]," count=",count
 		if str1[x]==str2[y] and str1[x+1]==str2[y+1]:
 			count+=1
 			x+=1
