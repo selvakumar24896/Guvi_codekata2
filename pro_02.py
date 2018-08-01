3ad4986d50e94b76707cec21c07ba114d9444c83
selvakumar24896 <selvakumar24896@gmail.com>
2018-08-01 15:47:08 +0530

    Update pro_02.py

--- pro_02.py
+++ pro_02.py
@@ -4,15 +4,15 @@ n=str(n)
 li=[]
 for i in n:
 	li.append(int(i))
-	
+
 while k>0:
 	temp=min(li)
 	ind=li.index(temp)
 	if k>=len(li[:ind]) and len(li[:ind])>0:
-		li.remove(li[ind-1])
+		li.remove(max(li[:ind]))
 		k-=1
 	else:
-		li.remove(li[len(li)-1])
+		li.remove(max(li[ind+1:]))
 		k-=1
 result=''
 for i in li:
