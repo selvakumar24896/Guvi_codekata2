177e7dfe4f4629bf0bf9046963fb6a96ba54a665
selvakumar24896 <selvakumar24896@gmail.com>
2018-08-02 10:33:55 +0530

    Update Pro_01.java

--- Pro_01.java
+++ Pro_01.java
@@ -1,37 +1,43 @@
+
 import java.util.*;
-public class Pro_01{
+
+public class Pro_01 {
 
     public static void main(String[] args) {
-        Scanner inp=new Scanner(System.in);
-        int n=inp.nextInt();
-        String[] names=new String[n];
-        for(int i=0;in;i++){
-            names[i]=inp.next();
-            
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        String[] names = new String[n];
+        for (int i = 0; in; i++) {
+            names[i] = inp.next();
+
         }
-     
-        String result=;
-        for(int i=0;inames.length-1;i++){
-            result=check(names[i],names[i+1]);
+
+        String result = ;
+        for (int i = 0; inames.length - 1; i++) {
+            result = check(names[i], names[i + 1]);
         }
         System.out.println(result);
     }
-    public static String check(String str1,String str2){
-        String out=,temp;
-        if(str1.length()str2.length()){
-            temp=str1;
-            str1=str2;
-            str2=temp;
+
+    public static String check(String str1, String str2) {
+        String out = , temp;
+        if (str1.length() {
+            str2.length()
         }
-        for(int i=0;istr1.length();i++){
-            
-                if(str1.charAt(i) == str2.charAt(i)){
-                    out+=str1.charAt(i);
-                }
-                else
-                    break;
+        
+            ){
+            temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+        for (int i = 0; istr1.length(); i++) {
+
+            if (str1.charAt(i) == str2.charAt(i)) {
+                out += str1.charAt(i);
+            } else {
+                break;
             }
-        return out;
         }
+        return out;
     }
-    
+}
