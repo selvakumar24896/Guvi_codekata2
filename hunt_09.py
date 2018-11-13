797b5b0e614c9da1c6ca0db0d779f50418093e90
selvakumar24896 <selvakumar24896@gmail.com>
2018-11-13 17:23:43 +0530

    Update hunt_09.py

--- hunt_09.py
+++ hunt_09.py
@@ -1,14 +1,16 @@
 def check(li):
+    
     for x in range(0,len(li)):
         for y in range(x+1,len(li)):
             if li[x]+li[y]==0:
                 return li[x],li[y]
+            
 n=input()
 li=[]
+
 if n>0:
     for x in range(0,n):
         li.append(input())
-    print check(li)
-    
+    print check(li)    
 else:
-    print "invalid input"   
+    print "INVALID input"   
