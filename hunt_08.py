4b1ceef87ee9b0a7f3c264bd1b00e769a1f27c2d
selvakumar24896 <selvakumar24896@gmail.com>
2018-11-13 17:22:19 +0530

    Update hunt_08.py

--- hunt_08.py
+++ hunt_08.py
@@ -1,21 +1,18 @@
 def check(li):
+    
     a=0
     b=0
-    out=[]
-    
+    out=[]    
     for x in range(0,len(li)-1):
         for y in range(x+1,len(li)-1):
             a=li[x]
             b=li[y]
             c=a+b
-            temp=li[y:]
-            
+            temp=li[y:]            
             if c in temp:
                 print a,b,c
                 
-                
-            
-
+                           
 n=input()
 if n>0:
     inp=[]
@@ -24,5 +21,5 @@ if n>0:
         inp.append(input())
     check(inp)
 else:
-    print("invalid input")
+    print("INVALID input")
         
