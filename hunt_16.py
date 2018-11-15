05bb698a6122f9a059020719834d3b1021b8ae24
selvakumar24896 <selvakumar24896@gmail.com>
2018-11-15 16:54:57 +0530

    Update hunt_16.py

--- hunt_16.py
+++ hunt_16.py
@@ -1,13 +1,16 @@
+li=[]
 n=input()
 k=input()
-li=[]
+
 for x in range(0,n):
 	li.append(input())
 	if k==li[x]:
 		ind=x
+out=[]
 l1=0
 l2=0
-out=[]
+
+
 for i in range(1,len(li)):
 	if ind-i  >= 0:
 		out.append(li[ind-i])
@@ -16,4 +19,5 @@ for i in range(1,len(li)):
 
 out=sorted(out)
 print out[:3]
+
 
