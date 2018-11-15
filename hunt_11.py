5b37229b174babc0c1de3cf80a9b9c787bf4494f
selvakumar24896 <selvakumar24896@gmail.com>
2018-11-15 16:50:07 +0530

    Update hunt_11.py

--- hunt_11.py
+++ hunt_11.py
@@ -1,21 +1,19 @@
+
 def rev(string):
     return string[::-1]+' '
 
 
-
-
 st=raw_input()
+
 result=''
 t=0
-
 
 for x in range(0,len(st)):
     if x==len(st)-1 or st[x+1]==' ':
         temp=st[t:x+1]
         result+=rev(temp.strip())
         t=x+1
-  
-    
+      
 print result
 
 
